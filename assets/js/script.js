00bf2ae376f63a0ac7a10f1e944a5361705bcd31
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-01-16 18:29:03 +0000

    Add global variables for script.js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  const APIKey = 'a443952c5b9202fc8b80f0ed06d94cec';
+  const citiesLogs = $('#cities-logs');
+  const todayCity = $('#today-city');
+  const todayIcon = $('#today-icon');
+  const todayTemperature = $('#today-tem');
+  const todayHumidity = $('#today-hum');
+  const todayWind = $('#today-win');
+});
+
+
+
+
+
